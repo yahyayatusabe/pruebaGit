cd434cb08fb9ebc475ba150baa6be001d9f8360c
yahya <a0x3572451e@s09-pc04.ausias.lan>
2020-02-07 18:50:23 +0100

    “Agregada la clase Operaciones”

---
+++ CalculoCaloriasGit./src/pruebaGit/Operaciones.java
@@ -0,0 +1,35 @@
+package pruebaGit;
+
+public class Operaciones {
+
+	private int operadorA;
+	private int operadorB;
+	
+	
+	public Operaciones( int operadorA, int operadorB) {
+		super();
+		this.operadorA=operadorA;
+		this.operadorB=operadorB;
+	}
+
+
+	public int getOperadorA() {
+		return operadorA;
+	}
+
+
+	public void setOperadorA(int operadorA) {
+		this.operadorA = operadorA;
+	}
+
+
+	public int getOperadorB() {
+		return operadorB;
+	}
+
+
+	public void setOperadorB(int operadorB) {
+		this.operadorB = operadorB;
+	}
+	
+}
